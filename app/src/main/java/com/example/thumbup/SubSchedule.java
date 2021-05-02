4cc683dc5be2ebef84f4976c5dc1d798eb60d943
songeun <71166220+simsongeun@users.noreply.github.com>
2021-05-02 12:48:16 +0900

    서브 스케줄

---
+++ app/src/main/java/com/example/thumbup/SubSchedule.java
@@ -0,0 +1,14 @@
+package com.example.thumbup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_schedule);
+    }
+}
