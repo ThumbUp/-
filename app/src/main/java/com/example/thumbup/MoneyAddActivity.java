b7e7f17bea40b128cd299b0cfb1f603501b5b95f
songeun <71166220+simsongeun@users.noreply.github.com>
2021-05-03 13:35:40 +0900

    (3-1)money_add / 파일(페이지) 생성

---
+++ app/src/main/java/com/example/thumbup/MoneyAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.thumbup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_add);
+    }
+}
