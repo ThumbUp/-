213f0a28f08e026631aaa5bc8b69c3a0e23dd0d2
oosiz <pg6272@gmail.com>
2021-05-08 18:33:42 +0900

    프로필 이미지 변경 & 갤러리 연결

--- app/src/main/java/com/example/thumbup/AfterActivity.java
+++ app/src/main/java/com/example/thumbup/AfterActivity.java
@@ -1,30 +1,47 @@
 package com.example.thumbup;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class AfterActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageButton btnRevoke, btnLogout;
+    ImageButton btnRevoke, btnLogout, btnGallery;
     TextView nameText, emailText;
     private FirebaseAuth mAuth ;
+
+    private Boolean isPermission = true;
+    private static final int PICK_FROM_ALBUM = 1;
+    private File tempFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
 
-        btnLogout = (ImageButton)findViewById(R.id.btn_logout);
-        nameText = (TextView)findViewById(R.id.myPage_nameText);
-        emailText = (TextView)findViewById(R.id.myPage_emailText);
+        btnLogout = (ImageButton) findViewById(R.id.btn_logout);
+        nameText = (TextView) findViewById(R.id.myPage_nameText);
+        emailText = (TextView) findViewById(R.id.myPage_emailText);
         //btnRevoke = (Button)findViewById(R.id.btn_revoke);
+        btnGallery = (ImageButton) findViewById(R.id.btn_ch_img);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -33,6 +50,18 @@ public class AfterActivity extends AppCompatActivity implements View.OnClickList
 
         nameText.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
         emailText.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        tedPermission();
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
+                if(isPermission) goToAlbum();
+                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     private void signOut() {
@@ -48,7 +77,7 @@ public class AfterActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.btn_logout:
                 signOut();
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
                 finish();
                 break;
@@ -58,4 +87,84 @@ public class AfterActivity extends AppCompatActivity implements View.OnClickList
 //                break;
         }
     }
+
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
+
+    private void goToAlbum() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            Uri photoUri = data.getData();
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                assert photoUri != null;
+                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        }
+    }
+
+    private void setImage() {
+
+        ImageView imageView = findViewById(R.id.profile_image);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        imageView.setImageBitmap(originalBm);
+
+    }
+
 }
