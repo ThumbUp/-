0ca2b74e5d3212b9df779af073821deac84e6e10
yujung7768903 <yu-jung31476@naver.com>
2021-05-12 02:04:46 +0900

    (전체)/하단바 생성, activity -> fragment 전환

--- app/src/main/java/com/example/thumbup/MainActivity.java
+++ app/src/main/java/com/example/thumbup/MainActivity.java
@@ -3,39 +3,113 @@ package com.example.thumbup;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    Button main_mainBtn;
+    Button main_meetingBtn;
+    Button main_moneyBtn;
+    Button main_loginBtn;
+    private final int mainFragment1 = 1;
+    private final int meetingFragment2 = 2;
+    private final int loginFragment4 = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main_mainBtn = (Button) findViewById(R.id.main_mainBtn);
+        main_meetingBtn = (Button) findViewById(R.id.main_meetingBtn);
+        main_moneyBtn = (Button) findViewById(R.id.main_moneyBtn);
+        main_loginBtn = (Button) findViewById(R.id.main_loginBtn);
 
-        findViewById(R.id.btnPopup).setOnClickListener(new View.OnClickListener() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        MainFragment mainFragment = new MainFragment();
+        transaction.replace(R.id.main_frame, mainFragment);
+        transaction.commit();
+
+        //main 탭 눌렀을 때
+        main_mainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(final View view) {
-                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
-                getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        if (menuItem.getItemId() == R.id.action_menu1){
-                            Toast.makeText(MainActivity.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
-                        }else if (menuItem.getItemId() == R.id.action_menu2){
-                            Toast.makeText(MainActivity.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
-                        }else {
-                            Toast.makeText(MainActivity.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
-                        }
-
-                        return false;
-                    }
-                });
-                popupMenu.show();
+            public void onClick(View v) {
+                FragmentView(mainFragment1);
             }
         });
+        //meeting 탭 눌렀을 때
+        main_meetingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentView(meetingFragment2);
+            }
+        });
+        //money 탭 눌렀을 때
+        main_moneyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        //mypage 탭 눌렀을 때
+        main_loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentView(loginFragment4);
+            }
+        });
+//        findViewById(R.id.btnPopup).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(final View view) {
+//                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+//                getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
+//                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//                    @Override
+//                    public boolean onMenuItemClick(MenuItem menuItem) {
+//                        if (menuItem.getItemId() == R.id.action_menu1){
+//                            Toast.makeText(MainActivity.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
+//                        }else if (menuItem.getItemId() == R.id.action_menu2){
+//                            Toast.makeText(MainActivity.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
+//                        }else {
+//                            Toast.makeText(MainActivity.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
+//                        }
+//
+//                        return false;
+//                    }
+//                });
+//                popupMenu.show();
+//            }
+//        });
+    }
+
+    private void FragmentView(int fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragment) {
+            case 1:
+                MainFragment mainFragment = new MainFragment();
+                transaction.replace(R.id.main_frame, mainFragment);
+                transaction.commit();
+                break;
+            case 2:
+                MeetingFragment meetingFragment = new MeetingFragment();
+                transaction.replace(R.id.main_frame, meetingFragment);
+                transaction.commit();
+                break;
+            case 3:
+                LoginFragment loginFragment = new LoginFragment();
+                transaction.replace(R.id.main_frame, loginFragment);
+                transaction.commit();
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, fragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/thumbup/MeetingFragment.java
+++ app/src/main/java/com/example/thumbup/MeetingFragment.java
@@ -1,31 +1,34 @@
 package com.example.thumbup;
 
 import android.os.Bundle;
-import android.widget.ListAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MeetingActivity extends AppCompatActivity {
+public class MeetingFragment extends Fragment {
     ListView meetingListView;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.meeting);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View meetingView = inflater.inflate(R.layout.meeting, container, false);
 
-        meetingListView = (ListView) findViewById(R.id.meeting_list);
+        meetingListView = (ListView) meetingView.findViewById(R.id.meeting_list);
         ArrayList<MeetingListViewItem> meetingListViewItem = new ArrayList<>();
 
-        String[] meetingListViewItem_date = {"4/1", "4/8", "4/15", "4/22", "4/29"};
+        String[] meetingListViewItem_date = {"4/1", "4/8", "4/15", "4/22", "4/29", "5/6", "5/13", "5/20"};
         String meetingListViewItem_name = "정기모임";
         String meetingListViewItem_time = "미정";
         String meetingListViewItem_place = "성신여대";
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 8; i++) {
             MeetingListViewItem item = new MeetingListViewItem();
             item.MeetingListViewItem_date = meetingListViewItem_date[i];
             item.MeetingListViewItem_name = meetingListViewItem_name;
@@ -36,5 +39,7 @@ public class MeetingActivity extends AppCompatActivity {
 
         MeetingAdapter meetingAdapter = new MeetingAdapter(meetingListViewItem);
         meetingListView.setAdapter(meetingAdapter);
+        return meetingView;
     }
+
 }
