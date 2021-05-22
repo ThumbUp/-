9cc50cbc36dfd345cd3b8eb7424c4f08f906ed86
seyoon <isy09440@gmail.com>
2021-05-22 12:49:04 +0900

    (1)main/모임 리스트뷰, 모임 생성&추가 창, 모임 설정 창, 모임 삭제 확인 다이얼로그 추가

--- app/src/main/java/com/example/thumbup/MainActivity.java
+++ app/src/main/java/com/example/thumbup/MainActivity.java
@@ -1,41 +1,101 @@
 package com.example.thumbup;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupMenu;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    ListView main_listView;
+    ImageButton btn_addMeeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnPopup).setOnClickListener(new View.OnClickListener() {
+        btn_addMeeting = (ImageButton)findViewById(R.id.btn_addMeeting);
+
+        MainActivity_ListAdapter adapter;
+        ArrayList<MainActivity_ItemData> items = new ArrayList<MainActivity_ItemData>() ;
+
+        adapter = new MainActivity_ListAdapter();
+
+        main_listView = (ListView)findViewById(R.id.main_listView);
+        main_listView.setAdapter(adapter);
+
+        for(int i = 1; i < 4; i++){
+            adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_profile), "여기 모임" + i, "스마트 모바일 프로그래밍 프로젝트 관련 모임" + i);
+        }
+
+        btn_addMeeting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
-                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
-                getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                final PopupMenu popupAdd = new PopupMenu(getApplicationContext(), view);
+                popupAdd.getMenuInflater().inflate(R.menu.meeting_add,popupAdd.getMenu());
+                popupAdd.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
                         if (menuItem.getItemId() == R.id.action_menu1){
-                            Toast.makeText(MainActivity.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
-                        }else if (menuItem.getItemId() == R.id.action_menu2){
-                            Toast.makeText(MainActivity.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
+                            final LinearLayout linear = (LinearLayout) View.inflate(MainActivity.this,
+                                    R.layout.dialog_main_add_meeting, null);
+
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setView(linear)
+                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+
+
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                    .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                    .show();
                         }else {
-                            Toast.makeText(MainActivity.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
+                            final LinearLayout linear = (LinearLayout) View.inflate(MainActivity.this,
+                                    R.layout.dialog_main_join_meeting, null);
+
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setView(linear)
+                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+
+
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                    .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                    .show();
                         }
 
                         return false;
                     }
                 });
-                popupMenu.show();
+                popupAdd.show();
             }
         });
     }
+
 }
