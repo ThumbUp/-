0ce21629315a3dca5ff14f9f8a4b52d3e717ce56
oosiz <pg6272@gmail.com>
2021-05-27 01:35:14 +0900

    mypage

--- app/src/main/java/com/example/thumbup/MeetingNoticeDialog.java
+++ app/src/main/java/com/example/thumbup/MeetingNoticeDialog.java
@@ -1,10 +1,8 @@
 package com.example.thumbup;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +11,6 @@ import androidx.annotation.NonNull;
 
 import com.example.thumbup.DataBase.DBCallBack;
 import com.example.thumbup.DataBase.DBManager;
-
-import java.util.ArrayList;
 
 public class MeetingNoticeDialog extends Dialog {
     public MeetingNoticeDialog(@NonNull Context context) {
@@ -42,7 +38,7 @@ public class MeetingNoticeDialog extends Dialog {
                 dbManager.UpdateMeeting("-MaZIcU6ZjxsYF_iX-6k", new DBCallBack() {
                     @Override
                     public void success(Object data) {
-                        ((MeetingFragment)getContext()).noticeList = dbManager.participatedMeetings.get("-MaZIcU6ZjxsYF_iX-6k").notices;
+//                        ((MeetingFragment)getContext()).noticeList = dbManager.participatedMeetings.get("-MaZIcU6ZjxsYF_iX-6k").notices;
                         dismiss();
                     }
 
