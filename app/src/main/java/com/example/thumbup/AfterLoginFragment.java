6e138219ef304ccacbd4261351d59da098bb3956
oosiz <pg6272@gmail.com>
2021-05-27 01:56:53 +0900

    mypage 2

--- app/src/main/java/com/example/thumbup/AfterLoginFragment.java
+++ app/src/main/java/com/example/thumbup/AfterLoginFragment.java
@@ -1,31 +1,46 @@
 package com.example.thumbup;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.example.thumbup.DataBase.DBCallBack;
-import com.example.thumbup.DataBase.DBManager;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.thumbup.DataBase.DBManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class AfterLoginFragment extends Fragment implements View.OnClickListener  {
     public static AfterLoginFragment newInstance() {
         return new AfterLoginFragment();
     }
-    ImageButton btnRevoke, btnLogout;
+    ImageButton btnRevoke, btnLogout, btnGallery;
     TextView nameText, emailText;
     private FirebaseAuth mAuth;
     private DBManager dbManager = DBManager.getInstance();
+
+    private Boolean isPermission = true;
+    private static final int PICK_FROM_ALBUM = 1;
+    private File tempFile;
 
     @Nullable
     @Override
@@ -37,6 +52,7 @@ public class AfterLoginFragment extends Fragment implements View.OnClickListener
         nameText = (TextView) afterLoginView.findViewById(R.id.myPage_nameText);
         emailText = (TextView) afterLoginView.findViewById(R.id.myPage_emailText);
         //btnRevoke = (Button)findViewById(R.id.btn_revoke);
+        btnGallery = (ImageButton) afterLoginView.findViewById(R.id.btn_ch_img);
 
         mAuth = FirebaseAuth.getInstance();
         //지금 하면 될까요? 넵! 로그인 했습니다!
@@ -50,6 +66,17 @@ public class AfterLoginFragment extends Fragment implements View.OnClickListener
         String email = user.getEmail();
         nameText.setText(user.getDisplayName());
         emailText.setText(user.getEmail());
+
+        tedPermission();
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
+                if(isPermission) goToAlbum();
+                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+            }
+        });
 
         return afterLoginView;
     }
@@ -72,5 +99,84 @@ public class AfterLoginFragment extends Fragment implements View.OnClickListener
                 break;
         }
     }
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+            }
+        };
+
+        TedPermission.with(getActivity())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
+
+    private void goToAlbum() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            Uri photoUri = data.getData();
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                assert photoUri != null;
+                cursor = getActivity().getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        }
+    }
+
+    private void setImage() {
+
+        ImageView imageView = (ImageView) getView().findViewById(R.id.profile_image);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        imageView.setImageBitmap(originalBm);
+
+    }
+
 }
 
